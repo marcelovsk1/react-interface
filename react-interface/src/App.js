b12c2a893dd7a78f21627100175d60e86d996177
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-08 17:02:54 -0300

    working on

--- react-interface/src/App.js
+++ react-interface/src/App.js
@@ -18,7 +18,15 @@ function App() {
           </button>
       </header>
       <div className="content-container">
-
+        <div className="image-container">
+          <img src="https://images.squarespace-cdn.com/content/v1/5cd2555994d71a16b3b6ffc1/c4013b7c-2f74-41f0-a11e-8b28a48b4be0/346898663_145124835115329_5428517613208174571_n.jpg" className="harold" alt="" />
+          <div className="slogan">
+            <h3>Back</h3>
+            <h3>to the originals</h3>
+            <p>with</p>
+            <h1>SATELLITE STROMPPERS</h1>
+          </div>
+        </div>
       </div>
     </main>
   );
