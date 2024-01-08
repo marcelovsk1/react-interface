4b9fd8dfe16fe9910155feb04916d75474ea699f
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-08 10:35:39 -0300

    working on

---
+++ react-interface/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </header>
+    </main>
+  );
+}
+
+export default App;
