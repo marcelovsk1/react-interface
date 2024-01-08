6601bbd8aecf124fb692a1bbefb3f41cc863e074
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-08 14:08:19 -0300

    working on

--- react-interface/src/App.js
+++ react-interface/src/App.js
@@ -4,13 +4,13 @@ function App() {
   return (
     <main>
       <header className="header-container">
-        <img src="./logo-summerly.svg" alt="" />
+        <img src="https://logodownload.org/wp-content/uploads/2019/11/adidas-originals-logo.png" className="logo-pic" alt="" />
         <nav className="menu-options">
-          <a href="#what-we-offer">What we offer</a>
-          <a href="#how-it-works">How it works</a>
-          <a href="#portfolio">Portfolio</a>
+          <a href="#what-we-offer">GAZELLE</a>
+          <a href="#how-it-works">SPEZIAL</a>
+          <a href="#portfolio">SAMBA</a>
         </nav>
-        <button>Contact us</button>
+        <button>Login In</button>
       </header>
     </main>
   );
